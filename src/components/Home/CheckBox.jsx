e42a4f002f313b72b18b2afd2e3c8e2cea57afac
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-09-15 17:35:46 +0300

    Some changes to the style are made

--- src/components/Home/CheckBox.jsx
+++ src/components/Home/CheckBox.jsx
@@ -3,10 +3,8 @@ import { BsCheck } from 'react-icons/bs';
 
 const CheckBox = ({isCompleted}) => {
   return (
-    <div className={`w-7 h-7 mr-3 border-2 rounded-lg border-pink-400 ${isCompleted ? 'bg-pink-400' : ''} overflow-hidden`}>
-      {isCompleted &&
-        <BsCheck size={24} className='text-zinc-800' />
-      }
+    <div className={`w-7 h-7 mr-3 border-2 rounded-lg overflow-hidden border-pink-400 ${isCompleted ? 'bg-pink-400' : ''}`}>
+      {isCompleted && <BsCheck size={24} className='text-zinc-800' />}
     </div>
   )
 }
